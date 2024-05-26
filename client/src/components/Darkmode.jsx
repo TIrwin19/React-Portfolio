e2cb45de5fdbbd88d29fcb7488034e235be7cdd8
Blackbox19 <trevorirwin819@gmail.com>
2024-05-25 23:46:35 -0400

    fixed dark mode button

---
+++ client/src/components/Darkmode.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import { MoonIcon, SunIcon } from "@heroicons/react/24/outline";
+
+export default function Darkmode() {
+  const [dark, setDark] = useState(false);
+
+  const darkModeHandler = () => {
+    setDark(!dark);
+    document.body.classList.toggle("dark");
+  };
+
+  return (
+    <div className="absolute z-30 md:top-7 md:right-6 bottom-4 right-2 items-center pr-2 ">
+      <button
+        onClick={() => darkModeHandler()}
+        type="button"
+        className=" rounded-full bg-zinc-900 dark:bg-gray-50 p-1 text-gray-400 dark:text-gray-700 hover:text-white drop-shadow hover:drop-shadow-md dark:hover:text-black focus:outline-none focus:ring-2 focus:ring-black dark:focus:ring-white transition ease-in-out hover:scale-105 "
+      >
+        {!dark ? (
+          <MoonIcon className="h-6 w-6" aria-hidden="true" />
+        ) : (
+          <SunIcon className="h-6 w-6" aria-hidden="true" />
+        )}
+      </button>
+    </div>
+  );
+}
